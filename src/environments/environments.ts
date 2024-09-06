9dcdbe93c9cd72a98bb0199f4ef4594e2a45144c
GajicMladen <mladengajic00@gmail.com>
2024-09-06 16:19:32 +0200

    fix: default values for enironments

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -3,7 +3,7 @@ console.log("here");
 console.log(window.env);
 
 export const environments={
-    user_service_url : window.env.USER_SERVICE_URL,
-    accommondation_service_url: window.env.ACCOMMODATION_SERVICE_URL,
-    reservation_service_url: window.env.RESERVATION_SERVICE_URL
+    user_service_url : window.env ? window.env.USER_SERVICE_URL : "http://localhost:8080/user",
+    accommondation_service_url: window.env ? window.env.ACCOMMODATION_SERVICE_URL : "http://localhost:8080/accommodation/",
+    reservation_service_url: window.env ? window.env.RESERVATION_SERVICE_URL : "http://localhost:8080/accommodatino/reservations/"
 }
